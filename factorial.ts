035de6b24f2788a1858642e3323acdf0cce58c6b
Jaan Jahilo <jaanjahilo@outlook.com>
2022-06-30 22:11:36 +0300

    Add docs to factorial.ts

--- factorial.ts
+++ factorial.ts
@@ -1,3 +1,8 @@
+/**
+ * Returns factorial of input
+ * @param {number} nr
+ * @returns {number} Factorial of nr
+ */
 export default (nr: number) => {
     let val = 1;
     for (let i = 2; i <= nr; i++) {
